782aff2f0d979ee3d545e9629f517acb2ce20de0
dan b <office@topsoft.ro>
2015-05-30 16:35:26 -0400

    type your commit message here

--- application/Ext/Include/SugarCRM_VOIP_ConnectorCE.php
+++ application/Ext/Include/SugarCRM_VOIP_ConnectorCE.php
@@ -1,5 +1,6 @@
 <?php 
  //WARNING: The contents of this file are auto-generated 11111111111aa2
+ ////////////1111111111111111
 $beanList['DMC_SugarCRM_VOIP_Connector'] = 'DMC_SugarCRM_VOIP_Connector';
 $beanFiles['DMC_SugarCRM_VOIP_Connector'] = 'modules/DMC_SugarCRM_VOIP_Connector/DMC_SugarCRM_VOIP_Connector.php';
 $modules_exempt_from_availability_check['DMC_SugarCRM_VOIP_Connector'] = 'DMC_SugarCRM_VOIP_Connector';
